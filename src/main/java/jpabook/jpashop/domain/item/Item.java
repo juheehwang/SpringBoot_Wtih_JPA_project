5176f7f1687a822cfe636784fab10fb3f6549f87
juhee <fhqj21@gmail.com>
2022-09-15 00:57:16 +0900

    add order service

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categoryList = new ArrayList<>();
+
+    /**
+     *
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     *
+     */
+    public void removeStock(int quantity){
+        int rest = this.stockQuantity - quantity;
+        if(rest<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = rest;
+    }
 }
